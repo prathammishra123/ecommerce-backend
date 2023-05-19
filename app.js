9f7a6ed4a79aec54d45d7c66ced3eff864343029
Pratham Mishra <prathammishra9867@gmail.com>
2023-05-19 05:31:48 +0530

    set cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(express.cookieParser())
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
     exposedHeaders: ["set-cookie"] 
 }));
